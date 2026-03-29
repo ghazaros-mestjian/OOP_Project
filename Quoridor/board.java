8b4f77e1102d21367ea9faa877a9fbaf6a36967a
Tatevik Tonoyan <tatevik_tonoyan@edu.aua.am>
2026-03-29 22:38:30 +0400

    Update board.java

--- Quoridor/board.java
+++ Quoridor/board.java
@@ -1,5 +1,12 @@
 public class Board {
+	//constants
+	private static final int HEIGHT = 9;
+	private static final int WIDTH  = 9;
+
+	
+	
 	public static void main(String[] args) {
+	
 		System.out.println("Hello!!");
 		// just for testing
 	}
