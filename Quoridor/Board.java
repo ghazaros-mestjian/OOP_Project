7ac8d1e9139ff56d6de931499a0743334c29ce3b
Tatevik Tonoyan <tatevik_tonoyan@edu.aua.am>
2026-04-19 16:28:02 +0400

    DFS & placeWall check

--- Quoridor/Board.java
+++ Quoridor/Board.java
@@ -36,5 +36,57 @@ public class Board {
 			return horizontalWalls[Math.min(x1, x2)][y1];
 	}
 
+    int[][] directions = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+    };
+    private void hasPath (int x, int y, boolean[][] isVisited) {
+        isVisited[x][y] = true;
 
+        for (int[] d : directions) {
+            int nx = d[0] + x;
+            int ny = d[1] + y;
+
+            if(!(checkWall(x, y, nx, ny)))
+                hasPath(nx, ny, isVisited);
+        }
+    }
+
+    public void canPlaceWall(int x, int y, boolean isVertical) {
+        // paty drecinq
+        if (isVertical) {
+            if (verticalWalls[x][y])
+                return;
+            verticalWalls[x][y] = true;
+        } else {
+            if (horizontalWalls[x][y])
+                return;
+            horizontalWalls[x][y] = true;
+        }
+
+        boolean[][] isVisited1 = new boolean[HEIGHT][WIDTH];
+        hasPath(piece1.getHeightCoordinate(), piece1.getWidthCoordinate(), isVisited1);
+        boolean[][] isVisited2 = new boolean[HEIGHT][WIDTH];
+        hasPath(piece2.getHeightCoordinate(), piece2.getWidthCoordinate(), isVisited2);
+
+        boolean flag1 = false;
+        boolean flag2 = false;
+        for (int i = 0; i < WIDTH; i++) {
+            if (isVisited1[HEIGHT-1][i])
+                flag1 = true;
+            if (isVisited2[0][i])
+                flag2 = true;
+        }
+
+        if (!flag1 || !flag2) {
+            //paty hanum enq
+            if (isVertical)
+                verticalWalls[x][y] = false;
+            else
+                horizontalWalls[x][y] = false;
+        }
+    }
 }
+
